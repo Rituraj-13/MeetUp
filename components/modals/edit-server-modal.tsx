f6d250892199c028b8c650b8b861a53af8f05e59
NoOne-777 <urgd2031@gmail.com>
2024-06-19 00:34:19 +0530

    added invitation and server settings

--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -4,7 +4,7 @@ import axios from "axios"
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form"
-
+import { useEffect } from "react";
 
 import {
     Dialog,
@@ -39,10 +39,11 @@ const formSchema = z.object({
 })
 
 export const EditServerModal = () => {
-    const { isOpen, onClose, type } = useModal();
+    const { isOpen, onClose, type, data } = useModal();
     const router = useRouter();
 
     const isModalOpen = isOpen && type === "editServer";
+    const { server } = data;
 
     const form = useForm({
         resolver: zodResolver(formSchema),
@@ -52,11 +53,19 @@ export const EditServerModal = () => {
         }
     });
 
+    useEffect(() => {
+        if (server){
+            form.setValue("name", server.name);
+            form.setValue("imageUrl", server.imageUrl);
+
+        }
+    }, [server, form]);
+
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            await axios.post("/api/servers", values);
+            await axios.patch(`/api/servers/${server?.id}`, values);
             
             form.reset();
             router.refresh();
@@ -126,7 +135,7 @@ export const EditServerModal = () => {
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
                             <Button disabled={isLoading} variant="primary">
-                                Create
+                                Save
                             </Button>
                         </DialogFooter>
                     </form>
