39da684d43134d1f7637b1e1a72649b7b1dc32ca
NoOne-777 <urgd2031@gmail.com>
2024-07-02 22:15:28 +0530

    added create channel functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -62,7 +62,9 @@ export const ServerHeader = ({
                 )}
                 {isModerator && (
                     <DropdownMenuItem
-                        className="px-3 py-2 text-sm cursor-pointer">
+                        onClick={() => onOpen("createChannel")}
+                        className="px-3 py-2 text-sm cursor-pointer"
+                    >
                         Create Channel
                         <PlusCircle className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
