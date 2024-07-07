2968fffe852555a58ab98ea52f5263fa7e03186a
NoOne-777 <urgd2031@gmail.com>
2024-07-07 14:35:09 +0530

    Added chat socket hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -3,9 +3,12 @@
 import { Fragment } from "react";
 import { format } from "date-fns";
 import { Member, Message, Profile } from "@prisma/client";
-import { ChatWelcome } from "./chat-welcome";
-import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
+
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { useChatSocket } from "@/hooks/use-chat-socket";
+
+import { ChatWelcome } from "./chat-welcome";
 import { ChatItem } from "./chat-item";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
@@ -40,7 +43,10 @@ export const ChatMessages = ({
     paramValue,
     type
 }: ChatMessagesProps) => {
-    const queryKey = `chat:${chatId}`
+    const queryKey = `chat:${chatId}`;
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey = `chat:${chatId}:messages:update`;
+
     const {
         data,
         fetchNextPage,
@@ -53,6 +59,7 @@ export const ChatMessages = ({
         paramKey,
         paramValue
     });
+    useChatSocket({ addKey, queryKey, updateKey});
 
     if (status === "pending") {
         return (
